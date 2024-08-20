e5d1fdc9bed4c38d45d26e7747be08eadd49664b
matheusdealcantara <dealcantaramatheus1@gmail.com>
2024-08-20 14:24:00 -0300

    Add new template files and change routes

--- route.py
+++ route.py
@@ -1,16 +1,25 @@
 from flask import Flask, request, render_template
 from app.controllers.application import Application
 from markupsafe import escape
+from helpers.helpers import login_required
 
 app = Flask(__name__)
 ctl = Application()
 
-@app.route
 
-@app.route("/")
-@app.route("/login")
+@app.route("/", methods=['GET', 'POST'])
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('layout.html')
+    if request.method == 'POST':
+        return render_template('index.html')
+    return render_template('login.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
